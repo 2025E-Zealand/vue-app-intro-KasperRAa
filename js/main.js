2a03f5370c88b616ed452879724fb6564bce5d6e
kaa004-zealand <kaa004@edu.zealand.dk>
2025-10-29 11:49:56 +0100

    More stuff

--- js/main.js
+++ js/main.js
@@ -2,14 +2,33 @@ const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
-            navn: 'David',
+            fornavn: 'David',
             efternavn: 'Smith',
-            vis_navn: false
+            vis_navn: false,
+            nytNavn: '',
+            nyAlder: 0,
+            navne: [
+                { navn: 'Jack', alder: 12 },
+                { navn: 'Jake', alder: 13 },
+                { navn: 'John', alder: 14 },
+                { navn: 'Jonkler', alder: 156 },
+            ],
         }
     },
     methods: {
         myMethod(){
 
+        },
+        addName(){
+            let newPerson = { navn:'', alder:0 }
+            newPerson.navn = this.nytNavn;
+            newPerson.alder = this.nyAlder;
+            this.navne.push(newPerson);
+            this.nytNavn = '';
+            this.nyAlder = 0;
+        },
+        emptyNames(){
+            this.navne = [];
         },
     },
     computed: {
